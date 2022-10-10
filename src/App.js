0bdd8b7f34be3a1d9d088c734228909c207052a7
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-10-10 16:09:26 +0100

    Implemented button functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentColor: "blue" };
+  }
+
+  onButtonClick = () => {
+    this.state.currentColor === "blue"
+      ? this.setState({ currentColor: "red" })
+      : this.setState({ currentColor: "blue" });
+  };
+
+  render() {
+    return (
+      <div>
+        <button
+          style={{ backgroundColor: this.state.currentColor }}
+          onClick={this.onButtonClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Change to {this.state.currentColor === "blue" ? "red" : "blue"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial text and color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole("button", { name: "Change to red" });
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+});
+
+test("button turns blue when clicked", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: "Change to red" });
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveStyle({ backgroundColor: "red" });
+  expect(buttonElement).toHaveTextContent("Change to blue");
 });
