e43f31b2775a560af5b648702fe86cb201ee2dcc
Gabriel Baldock <gabrielbaldock@gmail.com>
2022-10-10 16:22:30 +0100

    Implemented checkbox

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentColor: "blue" };
+    this.state = { currentColor: "blue", checked: true };
   }
 
   onButtonClick = () => {
@@ -13,15 +13,27 @@ class App extends React.Component {
       : this.setState({ currentColor: "blue" });
   };
 
+  onCheckboxClick = () => {
+    this.state.checked === true
+      ? this.setState({ checked: false })
+      : this.setState({ checked: true });
+  };
+
   render() {
     return (
       <div>
         <button
           style={{ backgroundColor: this.state.currentColor }}
           onClick={this.onButtonClick}
+          disabled={!this.state.checked}
         >
           Change to {this.state.currentColor === "blue" ? "red" : "blue"}
         </button>
+        <input
+          type="checkbox"
+          defaultChecked={this.state.checked}
+          onClick={this.onCheckboxClick}
+        />
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,24 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 import App from "./App";
 
+test("button is enabled at the beginning and checkbox checked", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: "Change to red" });
+  const checkboxElement = screen.getByRole("checkbox");
+
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).toBeChecked();
+});
+
+test("button is disabled when checkbox is ticked", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: "Change to red" });
+  const checkboxElement = screen.getByRole("checkbox");
+
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).not.toBeEnabled();
+});
+
 test("button has correct initial text and color", () => {
   render(<App />);
   const buttonElement = screen.getByRole("button", { name: "Change to red" });
